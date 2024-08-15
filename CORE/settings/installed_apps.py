930594c6f069dbbeeac6baccb5c7157016fae5f8
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-08-14 22:57:15 -0300

    Set up django-allauth

--- CORE/settings/installed_apps.py
+++ CORE/settings/installed_apps.py
@@ -11,4 +11,13 @@ PROJECT_APPS = [
     "accounts",
 ]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+EXTERNAL_APPS = [
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.facebook",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.apple",
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + EXTERNAL_APPS
