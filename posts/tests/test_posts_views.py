bdb6ebaa8b9984904ee4386f62f82582bf87b289
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-08-26 15:33:31 -0300

    Test post creation functionality

--- posts/tests/test_posts_views.py
+++ posts/tests/test_posts_views.py
@@ -1,7 +1,11 @@
+import os
+
 from django.test import TestCase
 from django.urls import reverse
 from posts.models import Post, Comment
 from django.contrib.auth import get_user_model
+from django.conf import settings
+
 
 from templates.static import notifications
 
@@ -10,6 +14,8 @@ User = get_user_model()
 
 class PostViewsTests(TestCase):
     def setUp(self):
+        image_path = os.path.join(settings.BASE_DIR, "media/coffee.jpg")
+
         user = User.objects.create_user(
             email="email@email.com",
             first_name="Test",
@@ -22,9 +28,19 @@ class PostViewsTests(TestCase):
             title="Lorem ipsum dolor sit amet.",
             excerpt="Lorem ipsum dolor sit amet.",
             content="Lorem ipsum dolor sit amet.",
-            image="media/coffee.png",
+            image="media/coffee.jpg",
             is_published=True,
         )
+
+        self.form_data = {
+            "title": "Lorem ipsum dolor sit amet",
+            "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ipsam incidunt praesentium accusantium veritatis ut ea esse laboriosam nulla, aliquid sapiente saepe distinctio officia eos placeat. Dicta ipsa ipsum velit.",
+            "image": open(image_path, "rb"),
+        }
+
+    def login_client(self):
+        self.client.login(email="email@email.com", password="testCASE123.")
 
     def test_list_view_returns_status_200_OK(self):
         response = self.client.get(reverse("posts:list_view"))
@@ -44,7 +60,7 @@ class PostViewsTests(TestCase):
         self.assertTemplateUsed(response, "posts/details.html")
 
     def test_comment_view_redirects_to_home_in_get_method(self):
-        self.client.login(email="email@email.com", password="testCASE123.")
+        self.login_client()
 
         url = reverse("posts:comment_view")
         expected_url = reverse("posts:list_view")
@@ -53,7 +69,7 @@ class PostViewsTests(TestCase):
         self.assertRedirects(response, expected_url)
 
     def test_comment_empty_raises_error_message(self):
-        self.client.login(email="email@email.com", password="testCASE123.")
+        self.login_client()
 
         url = reverse("posts:comment_view")
         response = self.client.post(
@@ -70,20 +86,81 @@ class PostViewsTests(TestCase):
         self.assertEqual(obj.count(), 0)
         self.assertIn(str(notifications.ERROR["comment_empty"]), html_content)
 
-    def test_comment_creation(self):
-        self.client.login(email="email@email.com", password="testCASE123.")
+    def test_comment_creation_success(self):
+        self.login_client()
 
         url = reverse("posts:comment_view")
+        comment_data = {
+            "post": self.post.id,
+            "comment": "This comment is good!",
+        }
+
         response = self.client.post(
             url,
-            {
-                "post": self.post.id,
-                "comment": "This comment is good!",
-            },
+            data=comment_data,
             follow=True,
         )
         html_content = response.content.decode("utf-8")
-        obj = Comment.objects.filter(post=self.post)
+        created_comment = Comment.objects.filter(
+            post=self.post, comment=comment_data["comment"]
+        )
 
-        self.assertEqual(obj.count(), 1)
+        self.assertTrue(created_comment.exists())
         self.assertIn(str(notifications.SUCCESS["comment_created"]), html_content)
+
+    def test_post_creation_view_returns_status_200_OK(self):
+        self.login_client()
+
+        url = reverse("posts:create_view")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_creation_forms_title_short_of_then_character_error(self):
+        self.login_client()
+
+        url = reverse("posts:create_view")
+        self.form_data["title"] = "123456789"
+        response = self.client.post(url, self.form_data, follow=True)
+        content = response.content.decode("utf-8")
+
+        self.assertIn(
+            str(notifications.ERROR["creation_post_title_short_of_ten_characters"]),
+            content,
+        )
+
+    def test_post_creation_forms_title_equal_excerpt_raises_error(self):
+        self.login_client()
+
+        url = reverse("posts:create_view")
+        self.form_data["title"] = (
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        )
+        self.form_data["excerpt"] = (
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        )
+
+        response = self.client.post(url, self.form_data, follow=True)
+        content = response.content.decode("utf-8")
+
+        self.assertIn(
+            str(notifications.ERROR["creation_post_title_equal_excerpt"]),
+            content,
+        )
+
+    def test_post_creation_success(self):
+        self.login_client()
+
+        url = reverse("posts:create_view")
+        response = self.client.post(
+            url,
+            data=self.form_data,
+            follow=True,
+        )
+
+        content = response.content.decode("utf-8")
+
+        post_created = Post.objects.filter(title=self.form_data["title"])
+
+        self.assertIn(str(notifications.SUCCESS["post_created"]), content)
+        self.assertTrue(post_created.exists())
