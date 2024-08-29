d087ef8acbce14adc000442d27ee4d68393b3063
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-08-29 16:49:38 -0300

    feature: develop edit post functionality

--- templates/static/notifications.py
+++ templates/static/notifications.py
@@ -6,9 +6,11 @@ ERROR = {
     "creation_post_title_short_of_ten_characters": _(
         "The title can be nothing short of 10 characters."
     ),
+    "not_post_author": _("You are not the author of this post."),
 }
 
 SUCCESS = {
     "comment_created": _("The comment was successfully posted!"),
     "post_created": _("The post was sent for verification!"),
+    "post_edited": _("Your post was edited and sent for verification!"),
 }
