75b0f139dff59a7794f11b201105c35b5c290a36
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-11-29 11:34:11 -0300

    refactor: abstract bookmark fetch functions to single file

--- frontend/src/components/PostDetails/BookmarkButtons.jsx
+++ frontend/src/components/PostDetails/BookmarkButtons.jsx
@@ -1,76 +1,24 @@
-import { useState } from "react";
-import { toast } from "sonner";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../store/auth-context.jsx";
+import { addBookmark, removeBookmark } from "../../utils/http.js";
 
-export default function BookmarkButtons({
-  initialIsBookmarked,
-  isAuthenticated,
-  postId,
-}) {
+export default function BookmarkButtons({ initialIsBookmarked, postId }) {
+  const { isLogged } = useAuth();
   const [isBookmarked, setIsBookmarked] = useState(initialIsBookmarked);
   const navigate = useNavigate();
 
-  async function addBookmark() {
-    if (!isAuthenticated) {
-      toast.info("You need to login to bookmark the post.");
-      return navigate(`/login?next=/post/${postId}`);
-    }
-
-    try {
-      const response = await fetch(
-        `http://localhost:8000/bookmarks/api/create/${postId}`,
-        {
-          method: "POST",
-          credentials: "include",
-        },
-      );
-      const data = await response.json();
-
-      if (!response.ok) {
-        toast.error(
-          data.message || "Something went wrong while bookmarking the post.",
-        );
-        return;
-      }
-
-      toast.success(data.message);
-      setIsBookmarked(true);
-    } catch {
-      toast.error("An unexpected error occurred. Please, try again.");
-    }
-  }
-
-  async function removeBookmark() {
-    try {
-      const response = await fetch(
-        `http://localhost:8000/bookmarks/api/delete/${postId}`,
-        {
-          method: "delete",
-          credentials: "include",
-        },
-      );
-      const data = await response.json();
-
-      if (!response.ok) {
-        toast.error(
-          data.message || "Something went wrong while removing the bookmark.",
-        );
-        return;
-      }
-
-      toast.warning(data.message);
-      setIsBookmarked(false);
-    } catch (err) {
-      console.log(err);
-      toast.error("An unexpected error occurred. Please, try again.");
-    }
-  }
+  useEffect(() => {
+    setIsBookmarked(initialIsBookmarked);
+  }, [initialIsBookmarked, postId]);
 
   return (
     <>
       {!isBookmarked ? (
         <button
-          onClick={addBookmark}
+          onClick={() =>
+            addBookmark(postId, isLogged, navigate, setIsBookmarked)
+          }
           className="flex items-center  gap-1 text-base duration-300 hover:text-[#5b4a3e]"
         >
           <ion-icon name="bookmark-outline"></ion-icon>
@@ -78,8 +26,8 @@ export default function BookmarkButtons({
         </button>
       ) : (
         <button
-          onClick={removeBookmark}
-          className="flex items-center  gap-1 text-base duration-300 hover:text-[#5b4a3e]"
+          onClick={() => removeBookmark(postId, setIsBookmarked)}
+          className="flex items-center  gap-1 text-base duration-300 hover:text-red-600"
         >
           <ion-icon name="bookmark"></ion-icon>
           <span>Remove bookmark</span>
