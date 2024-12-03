390270a9c2359e1984a48c829b73eb28a9e724a0
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-12-02 22:29:05 -0300

    fix: add header and footer on GenericError component

---
+++ frontend/src/pages/GenericError.jsx
@@ -0,0 +1,23 @@
+import MainHeader from "../components/MainHeader";
+import Footer from "../components/Footer";
+
+import { Link } from "react-router-dom";
+import BaseError from "../components/BaseError";
+
+export default function GenericError() {
+  return (
+    <>
+      <MainHeader />
+      <BaseError title="Ops! We couldn't find this page, sorry!">
+        <p>
+          However, you can get back to the{" "}
+          <Link to="/" className="text-[#AB886D]">
+            home page
+          </Link>
+          !
+        </p>
+      </BaseError>
+      <Footer />
+    </>
+  );
+}
