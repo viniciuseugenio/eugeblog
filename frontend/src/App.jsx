99f8a6cb42299f7b1bf803abf12fc58f0a041bf3
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-01-02 11:28:31 -0300

    refactor: replace loader by custom hook to check user auth in ContextWrapper component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import ContextWrapper, { loader as authLoader } from "./pages/ContextWrapper";
+import ContextWrapper from "./pages/ContextWrapper";
 import GenericError from "./pages/GenericError.jsx";
 import HomePage from "./pages/Home";
 import LoginPage, {
@@ -20,7 +20,6 @@ import { CircularProgress } from "@mui/material";
 const router = createBrowserRouter([
   {
     element: <ContextWrapper />,
-    loader: authLoader,
     hydrateFallbackElement: <CircularProgress />,
     children: [
       {
