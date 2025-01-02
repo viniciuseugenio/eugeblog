86d683d35f9b97f49538f3520f3eee7e5492baa9
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-01-02 15:55:55 -0300

    fix: export auth.js functions in http/index.js

--- frontend/src/utils/http/index.js
+++ frontend/src/utils/http/index.js
@@ -1,5 +1,6 @@
 export * from "./post.js";
 export * from "./bookmarks.js";
+export * from "./auth.js";
 
 import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient();
