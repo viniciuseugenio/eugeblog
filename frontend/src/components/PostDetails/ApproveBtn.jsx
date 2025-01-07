bfd2a73250cdc70be4042e38cc5a28fd22caf406
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-01-07 18:12:06 -0300

    refactor(approvebtn): simplify post approval logic and remove unnecessary props

--- frontend/src/components/PostDetails/ApproveBtn.jsx
+++ frontend/src/components/PostDetails/ApproveBtn.jsx
@@ -1,27 +1,30 @@
 import { useMutation } from "@tanstack/react-query";
 import { useContext, useRef } from "react";
-import { PostDetailsContext } from "./PostDetailsBase";
-import { acceptPostReview } from "../../utils/http";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
+import { acceptPostReview, queryClient } from "../../utils/http";
 import Modal from "../Modal";
-import BaseModalButtons from "../BaseModalButtons";
+import { PostDetailsContext } from "./PostDetailsBase";
 
-export default function ApproveBtn({
-  handleSuccess,
-  handleError,
-  buttonClasses,
-}) {
+export default function ApproveBtn({ buttonClasses }) {
   const { postId } = useContext(PostDetailsContext);
   const modal = useRef();
-
-  const handleApproval = () => {
-    modal.current.showModal();
-  };
+  const navigate = useNavigate();
 
   const { mutate } = useMutation({
     mutationFn: acceptPostReview,
-    onSuccess: () =>
-      handleSuccess("This post was accepted and published successfully."),
-    onError: handleError,
+    onSuccess: () => {
+      toast.success("This post was successfully approved.", {
+        id: "approve-success",
+      });
+      queryClient.invalidateQueries(["posts"]);
+      navigate("/");
+    },
+    onError: (error) => {
+      toast.error(error.message, {
+        id: "approve-error",
+      });
+    },
   });
 
   return (
@@ -29,21 +32,17 @@ export default function ApproveBtn({
       <Modal
         ref={modal}
         title="This post will be published!"
-        actionButtons={
-          <BaseModalButtons
-            confirmColor="text-light bg-green-800"
-            onClick={() => mutate(postId)}
-          />
-        }
+        mutateFn={() => mutate(postId)}
         icon="checkmark-circle-outline"
         iconColor="text-green-800"
+        confirmBtnClasses="ring-300 bg-green-200 text-green-950 shadow-md ring-1 hover:bg-green-300"
       >
         Please review all post content before approving it. We wouldn&apos;t
         want our users reading inappropriate content.
       </Modal>
 
       <button
-        onClick={handleApproval}
+        onClick={() => modal.current.showModal()}
         className={`${buttonClasses} text-green-800 shadow-lg ring-1 ring-green-200 hover:bg-green-200 hover:ring-green-300`}
       >
         <ion-icon name="checkmark-circle-outline"></ion-icon>
