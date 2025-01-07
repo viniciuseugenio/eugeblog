dd06660e263cc06a8cd539ba72674fca79699042
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-01-07 17:32:47 -0300

    fix: correct name site in HeroSection

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
   return (
     <div className="border-t-[1px] border-gray-300 bg-[#E4E0E1] py-8 text-center">
       <h1 className="text-xl font-bold">
-        &ldquo;Stay Informed, Stay Ahead — Your Daily News with Eugenda&rdquo;
+        &ldquo;Stay Informed, Stay Ahead — Your Daily News with Eugeblog&rdquo;
       </h1>
     </div>
   );
