c8ce1f6dd8c24d09629279b64cbce59be329bf62
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-01-13 20:06:49 -0300

    refactor(login): use custom hook to display error in Login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,29 +1,27 @@
 import { useMutation } from "@tanstack/react-query";
-import { useEffect, useRef } from "react";
-import { Link, useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router";
 import { toast } from "sonner";
 import logoImg from "../assets/eu-icon.svg";
 import Input from "../components/Input";
 import PrimaryButton from "../components/PrimaryButton";
 import SocialLogin from "../components/SocialLogin";
 import { useAuthContext } from "../store/auth-context";
-import { useAuthCheck } from "../utils/hooks";
-import { loginUser } from "../utils/http";
-import { queryClient } from "../utils/http";
+import { useAuthCheck, useSocialErrorDisplay } from "../utils/hooks";
+import { loginUser, queryClient } from "../utils/http";
 
 export default function LoginPage() {
-  const location = useLocation();
-  const errorNotified = useRef(false);
+  useSocialErrorDisplay();
+
   const navigate = useNavigate();
   const { login: loginContext } = useAuthContext();
-
   const { data: authData } = useAuthCheck();
 
   useEffect(() => {
     if (authData?.isAuthenticated) {
       navigate("/");
     }
-  }, [authData, navigate]);
+  }, [authData?.isAuthenticated, navigate]);
 
   const {
     mutate: login,
@@ -45,27 +43,6 @@ export default function LoginPage() {
     const data = new FormData(event.target);
     login(data);
   }
-
-  useEffect(() => {
-    if (errorNotified.current) return;
-
-    const queryParams = new URLSearchParams(location.search);
-    const error = queryParams.get("error");
-    const errorMessage = {
-      access_denied:
-        "You have denied access to your account. Please, try again.",
-      email_fetching:
-        "An error occurred while fetching your e-mail. Please, try again.",
-      access_token: "Failed to obtain access token from Google.",
-    };
-
-    if (error) {
-      toast.error(
-        errorMessage[error] || "An error occurred. Please, try again.",
-      );
-      errorNotified.current = true;
-    }
-  }, [location]);
 
   return (
     <div className="flex h-screen items-center justify-center">
