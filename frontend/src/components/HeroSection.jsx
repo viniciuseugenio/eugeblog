229dd0e0b9718fe9988a143f2e4fb45ec75c3070
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-01-20 16:12:13 -0300

    style(herosection): use tailwind color instead of hard-coded hex code

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 export default function HeroSection() {
   return (
-    <div className="border-t-[1px] border-gray-300 bg-[#E4E0E1] py-8 text-center">
+    <div className="bg-light border-t-[1px] border-gray-300 py-8 text-center">
       <h1 className="text-xl font-bold">
         &ldquo;Stay Informed, Stay Ahead — Your Daily News with Eugeblog&rdquo;
       </h1>
