c67b035e07ae44c40161ddef855c9c5c703232c0
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-02-11 18:30:42 -0300

    style(logged-header): update styling and order of buttons
    - Update dropdown icons
    - Remove LogoutButton (moved to MainHeader)
    - Remove tooltip in "New Post" button in favor of button text for better UI
    - Delete deprecated liClasses variable
    - Update "New Post" button style to match Dropdown buttons
    - Update the order of the buttons

--- frontend/src/components/Header/LoggedHeader.jsx
+++ frontend/src/components/Header/LoggedHeader.jsx
@@ -2,41 +2,38 @@ import { Link } from "react-router";
 import BookmarksDropdown from "./BookmarksDropdown";
 import Dropdown from "./Dropdown";
 import IconSpan from "./IconSpan";
-import LogoutButton from "./LogoutButton";
 import UserPostsDropdown from "./UserPostsDropdown";
-import Tooltip from "../Tooltip";
 
 export default function LoggedHeader() {
-  const liClasses =
-    "active:bg-accent flex items-center justify-center rounded-lg bg-[#e2d3ca] px-4 duration-300 hover:bg-[#decdc2] hover:text-black";
-
   return (
-    <>
-      <Link to="/post/create/" className={`${liClasses} group relative`}>
-        <li>
-          <IconSpan>
-            <ion-icon name="add-outline"></ion-icon>
-          </IconSpan>
-        </li>
-        <Tooltip text="Create a post" topPosition="top-11" />
-      </Link>
+    <ul className="flex gap-3">
       <li>
         <Dropdown
           label="User Posts"
-          icon="file-tray-stacked-outline"
+          icon="person-outline"
           DropdownContent={UserPostsDropdown}
         />
       </li>
+
       <li>
         <Dropdown
           label="Bookmarks"
-          icon="bookmarks-outline"
+          icon="bookmark-outline"
           DropdownContent={BookmarksDropdown}
         />
       </li>
-      <li className={liClasses}>
-        <LogoutButton />
-      </li>
-    </>
+
+      <Link
+        to="/post/create/"
+        className="active:bg-accent group relative flex items-center justify-center rounded-lg px-4 duration-300 hover:bg-[#e2d3ca] hover:text-black"
+      >
+        <li className="flex gap-1">
+          <IconSpan>
+            <ion-icon name="add-outline"></ion-icon>
+          </IconSpan>
+          <p className="text-sm">New post</p>
+        </li>
+      </Link>
+    </ul>
   );
 }
