0d2f709ffc5c005c8d7376fe2edafae453c8c95d
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-02-21 21:13:52 -0300

    feat(comment): add tabIndex and display deletion button on focus for better accessibility

--- frontend/src/components/PostDetails/Comment.jsx
+++ frontend/src/components/PostDetails/Comment.jsx
@@ -1,7 +1,7 @@
-import { formatDate } from "../../utils/helpers";
 import { CircleUser } from "lucide-react";
-import CommentDeleteButton from "./CommentDeleteButton";
 import { useAuthContext } from "../../store/auth-context";
+import { formatDate } from "../../utils/helpers";
+import CommentDeleteButton from "./CommentDeleteButton";
 
 export default function Comment({ comment }) {
   const createdAt = formatDate(comment.created_at);
@@ -13,9 +13,12 @@ export default function Comment({ comment }) {
     ? "grid-cols-[2.4rem_1fr_2.6rem]"
     : "grid-cols-[2.4rem_1fr]";
 
+  const deleteButtonStates =
+    "group-focus-within:visible group-focus-within:scale-100 group-focus-within:opacity-100 group-hover:visible group-hover:scale-100 group-hover:opacity-100";
 
   return (
     <div
+      tabIndex={0}
       className={`hover:bg-light/40 group grid ${columns} grid-rows-[1.6rem_1fr] rounded-md px-2 py-1 duration-300`}
     >
       <span className="row-span-2 self-start p-1">
@@ -29,7 +32,9 @@ export default function Comment({ comment }) {
         {comment.content}
       </p>
       {isAuthor && (
-        <div className="invisible row-span-2 ml-1 scale-0 self-center opacity-0 duration-300 group-hover:visible group-hover:scale-100 group-hover:opacity-100">
+        <div
+          className={`invisible row-span-2 ml-1 scale-0 self-center opacity-0 duration-300 ${deleteButtonStates}`}
+        >
           <CommentDeleteButton commentId={comment.id} />
         </div>
       )}
