669f16dcf029824c086e4e9736472460c5dc5e3d
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-02-25 19:36:40 -0300

    style(tooltip): improve hover transition and opacity handling
    - Add `origin-bottom` to ensure scaling transitions from the bottom
    - Introduce `opacity-0` to smoothly handle appearance
    - Use `transition-all` for consistent transformations
    - Delay appearance by 500ms (`group-hover: delay-500`)

--- frontend/src/components/Tooltip.jsx
+++ frontend/src/components/Tooltip.jsx
@@ -1,7 +1,7 @@
 export default function Tooltip({ text, topPosition }) {
   return (
     <span
-      className={`${topPosition} absolute left-1/2 z-20 -translate-x-1/2 scale-0 transform whitespace-nowrap rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-bold shadow-md transition-transform duration-300 ease-in-out group-hover:scale-100 group-[.open]:scale-0`}
+      className={`${topPosition} absolute left-1/2 z-20 origin-bottom -translate-x-1/2 scale-0 whitespace-nowrap rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-bold opacity-0 shadow-md transition-all duration-200 ease-in-out group-hover:scale-100 group-hover:opacity-100 group-hover:delay-500 group-[.open]:scale-0`}
     >
       {text}
     </span>
