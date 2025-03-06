e4a13ba7462b6b9758d9f22ee069616ff2e40acf
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-03-06 14:34:35 -0300

    refactor(comment-input): lift state and relocate buttons to `CommentForm`

--- frontend/src/components/PostDetails/CommentForm.jsx
+++ frontend/src/components/PostDetails/CommentForm.jsx
@@ -1,22 +1,27 @@
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { Link, useParams } from "react-router";
 import { toast } from "sonner";
 import { useAuthContext } from "../../store/auth-context.jsx";
 import { createComment, queryClient } from "../../utils/api/index.js";
+import PrimaryButton from "../PrimaryButton.jsx";
 import CommentInput from "./CommentInput";
 
 export default function CommentForm() {
   const { id: postId } = useParams();
   const { isLogged } = useAuthContext();
+  const [value, setValue] = useState();
 
-  const { mutate, isPending, isSuccess, reset } = useMutation({
+  const { mutate, isPending } = useMutation({
     mutationFn: createComment,
     onSuccess: (data) => {
       if (!data.detail) {
         data.content.map((error) => toast.error(error));
+        document.getElementById("comment_content").focus();
         return;
       }
 
+      setValue("");
       toast.success("Comment posted successfully.");
       queryClient.invalidateQueries(["comments", postId]);
     },
@@ -45,11 +50,33 @@ export default function CommentForm() {
   if (isLogged) {
     return (
       <form onSubmit={handleCommentCreation} method="post">
-        <CommentInput
-          isPending={isPending}
-          isSuccess={isSuccess}
-          mutationReset={reset}
-        />
+        <div className="group/div">
+          <CommentInput setValue={setValue} value={value} />
+
+          <div className="mb-6 flex justify-end">
+            <div
+              className={`${isPending ? "visible scale-100 opacity-100" : "invisible scale-90 opacity-0"} flex origin-center transform gap-3 duration-300 ease-in-out group-focus-within/div:visible group-focus-within/div:scale-100 group-focus-within/div:opacity-100`}
+            >
+              <button
+                type="reset"
+                onClick={() => {
+                  setValue("");
+                  document.activeElement.blur();
+                }}
+                className="active:bg-accent bg-accent/50 hover:bg-accent/70 ring-primary/30 hover:ring-primary/60 active:ring-primary/90 rounded-lg px-4 py-1 ring-1 duration-300 active:scale-95"
+                disabled={isPending}
+              >
+                Cancel
+              </button>
+              <PrimaryButton
+                text="Comment"
+                type="submit"
+                isPending={isPending}
+                className="px-6 active:scale-90"
+              />
+            </div>
+          </div>
+        </div>
       </form>
     );
   } else {
--- frontend/src/components/PostDetails/CommentInput.jsx
+++ frontend/src/components/PostDetails/CommentInput.jsx
@@ -1,15 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
-export default function CommentInput({ isPending, isSuccess, mutationReset }) {
+export default function CommentInput({ value, setValue }) {
   const textArea = useRef();
-  const [value, setValue] = useState("");
-
-  useEffect(() => {
-    if (isSuccess && value) {
-      setValue("");
-      mutationReset();
-    }
-  }, [isSuccess, value, mutationReset]);
 
   function handleChange(event) {
     setValue(event.target.value);
@@ -21,40 +13,15 @@ export default function CommentInput({ isPending, isSuccess, mutationReset }) {
   }, [value]);
 
   return (
-    <div className="group/div">
-      <textarea
-        ref={textArea}
-        name="content"
-        id="content"
-        rows={1}
-        value={value}
-        placeholder="Add a comment..."
-        onChange={handleChange}
-        className="mb-2 w-full border-b border-[#AB886D] pb-1 outline-none duration-300 focus:border-[#493628]"
-      />
-
-      <div className="mb-6 flex justify-end">
-        <div className="invisible flex origin-center scale-90 transform gap-3 opacity-0 duration-300 ease-in-out group-focus-within/div:visible group-focus-within/div:scale-100 group-focus-within/div:opacity-100">
-          <button
-            type="reset"
-            onClick={() => {
-              setValue("");
-              document.activeElement.blur();
-            }}
-            className="active:bg-accent bg-accent/50 hover:bg-accent/70 ring-primary/30 hover:ring-primary/60 active:ring-primary/90 rounded-lg px-4 py-1 ring-1 duration-300 active:scale-95"
-            disabled={isPending}
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            disabled={isPending}
-            className="group/button active:bg-primary rounded-lg bg-[#493E37] px-6 py-1.5 text-white duration-300 hover:bg-[#3d342f] hover:shadow-lg active:scale-95 active:shadow-sm"
-          >
-            <p className="duration-300 ease-out">Comment</p>
-          </button>
-        </div>
-      </div>
-    </div>
+    <textarea
+      ref={textArea}
+      name="content"
+      id="content"
+      rows={1}
+      value={value}
+      placeholder="Add a comment..."
+      onChange={handleChange}
+      className="mb-2 w-full border-b border-[#AB886D] pb-1 outline-none duration-300 focus:border-[#493628]"
+    />
   );
 }
