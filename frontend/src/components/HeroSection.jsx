ccdb633d09e4173ce1989a4c425b75126cdc731c
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-03-24 16:21:33 -0300

    feat(hero-section): update styling and add text
    - Make main heading bigger
    - Add a smaller supporting text

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,9 +1,16 @@
 export default function HeroSection() {
   return (
-    <div className="bg-light border-t-[1px] border-gray-300 py-8 text-center">
-      <h1 className="text-xl font-bold">
-        &ldquo;Stay Informed, Stay Ahead — Your Daily News with Eugeblog&rdquo;
-      </h1>
-    </div>
+    <section className="bg-light border-t-[1px] border-gray-300 py-16 sm:py-24">
+      <div className="container mx-auto px-4 text-center sm:px-6 md:px-8">
+        <h1 className="mb-6 text-3xl font-bold sm:text-5xl">
+          &ldquo;Stay Informed, Stay Ahead — Your Daily News with
+          Eugeblog&rdquo;
+        </h1>
+        <p className="xs:text-xl mx-auto max-w-3xl text-lg text-opacity-70 opacity-70">
+          Discover thoughtful insights, expert analysis, and the latest trends
+          across various topics.
+        </p>
+      </div>
+    </section>
   );
 }
