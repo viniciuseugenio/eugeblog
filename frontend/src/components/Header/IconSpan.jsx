8ed66d5d0a531812b3154799ed1f6fee39388dcb
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-03-24 17:35:20 -0300

    feat(icon-span): accept `className` as a prop

--- frontend/src/components/Header/IconSpan.jsx
+++ frontend/src/components/Header/IconSpan.jsx
@@ -1,11 +1,11 @@
 import { motion } from "motion/react";
 
-export default function IconSpan({ children, isOpen }) {
+export default function IconSpan({ children, isOpen, className }) {
   return (
     <motion.span
       animate={{ rotate: isOpen ? 180 : 0 }}
       transition={{ type: "spring", bounce: 0.5 }}
-      className="flex items-center justify-center"
+      className={`${className} flex items-center justify-center`}
     >
       {children}
     </motion.span>
